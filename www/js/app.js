66946eeaa1bd33c35dfd51a35ba2cd2bf8750f19
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2016-09-05 13:31:46 -0700

    Revises handling of ionic and non-ionic controllers and run statements

--- www/js/app.js
+++ www/js/app.js
@@ -8,18 +8,47 @@
 /*global ngModule*/
 /*global window*/
 /*global cordova*/
-
+/*global navigator*/
 
 var modules = [],
     dependencies = ['ui.router', 'ionic'],
-    isMobile = typeof ionic !== 'undefined' && (ionic.Platform.is("ios") || ionic.Platform.is("android"));
+    isMobile = typeof ionic !== 'undefined' && (ionic.Platform.is("ios") || ionic.Platform.is("android")),
+    div,
+    map,
+    mapModule;
 if (isMobile) {
     dependencies.push('ngCordova');
 }
 console.debug(modules);
 console.debug(dependencies);
 
-angular.module('maplinkr', dependencies.concat(modules))
+function toFixedOne(val, prec) {
+    "use strict";
+    var precision = prec || 0,
+        neg = val < 0,
+        power = Math.pow(10, precision),
+        value = Math.round(val * power),
+        integral = String((neg ? Math.ceil : Math.floor)(value / power)),
+        fraction = String((neg ? -value : value) % power),
+        padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0'),
+        sign = neg ? "-" : "";
+
+    if (integral[0] === '-') {
+        sign = "";
+    }
+    return sign + (precision ? integral + '.' +  padding + fraction : integral);
+}
+
+function toFixedTwo(x, y, precision) {
+    "use strict";
+    var fixed = {
+        lon : toFixedOne(x, precision),
+        lat : toFixedOne(y, precision)
+    };
+    return fixed;
+}
+
+mapModule = angular.module('maplinkr', dependencies.concat(modules))
 
     .config(['$locationProvider', '$compileProvider', '$urlRouterProvider', '$stateProvider',
         function ($locationProvider, $compileProvider, $urlRouterProvider, $stateProvider) {
@@ -38,118 +67,126 @@ angular.module('maplinkr', dependencies.concat(modules))
             });
 
             $urlRouterProvider.otherwise("/");
-        }])
-    .controller('MapCtrl', function ($scope, $state, $cordovaGeolocation) {
+        }]);
+
+if (isMobile) {
+    mapModule.controller('MapCtrl', function ($scope, $state, $cordovaGeolocation) {
         "use strict";
-        var options = {
-            timeout: 10000,
-            enableHighAccuracy: true
-        };
+        var div,
+            map,
+            options = {
+                timeout: 10000,
+                enableHighAccuracy: true
+            };
 
-        if (isMobile) {
-            $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
-                var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-                    mapOptions = {
-                        center: latLng,
-                        zoom: 15,
-                        mapTypeId: google.maps.MapTypeId.ROADMAP
-                    };
+        $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
+            var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                mapOptions = {
+                    center: latLng,
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
 
-                $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-            }, function (error) {
-                console.log("Could not get location");
-                console.debug(error);
-            });
-        } else {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    var pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
-                    // LinkrService.hideLinkr();
-                    mph.geoLocate(pos);
-                    // $scope.$parent.accept();
-                    setTimeout(function () {
-                        $scope.$parent.accept();
-                    }, 200);
-                },
-                    function () {
-                        handleLocationError(true, infoWindow, map.getCenter());
-                    });
-            } else {
-              // Browser doesn't support Geolocation
-                handleLocationError(false, infoWindow, map.getCenter());
-            }
+        }, function (error) {
+            console.log("Could not get location");
+            console.debug(error);
+        });
+
+        function toPluginPosition(lat, lng) {
+            return new plugin.google.maps.LatLng(lat, lng);
         }
-    })
-    .run(function ($ionicPlatform, $rootScope) {
+
+        div = document.getElementById("map");
+
+            // Invoking Map using Google Map SDK v2 by dubcanada
+        map = plugin.google.maps.Map.getMap(div, {
+            'camera': {
+                'latLng': toPluginPosition(-19.9178713, -43.9603117),
+                'zoom': 10
+            }
+        });
+    });
+} else {
+    mapModule.controller('MapCtrl', function ($scope, $state) {
         "use strict";
+
+        function formatCoords(pos) {
+            var fixed = toFixedTwo(pos.lng, pos.lat, 5),
+                formatted  = '<div style="color: blue;">' + fixed.lon + ', ' + fixed.lat + '</div>';
+            return formatted;
+        }
+
+        function geoLocate(pos) {
+            var infoWindow = new google.maps.InfoWindow({map: map});
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(formatCoords(pos));
+            console.log('geoLocate just happened at ' + pos.lng + ", " +  pos.lat);
+        }
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infoWindow.setPosition(pos);
+            infoWindow.setContent(browserHasGeolocation ?
+                    'Error: The Geolocation service failed.' :
+                    'Error: Your browser doesn\'t support geolocation.');
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+                // LinkrService.hideLinkr();
+                geoLocate(pos);
+            },
+                function () {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
+}
+
+if (isMobile) {
+    mapModule.run(function ($ionicPlatform, $window) {
+        "using strict";
         $ionicPlatform.ready(function () {
-            if (window.cordova && window.cordova.plugins.Keyboard) {
+            "using strict";
+            if ($window.cordova && $window.cordova.plugins.Keyboard) {
                 // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
                 // for form inputs)
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
                 // Don't remove this line unless you know what you are doing. It stops the viewport
                 // from snapping when text inputs are focused. Ionic handles this internally for
                 // a much nicer keyboard experience.
-                cordova.plugins.Keyboard.disableScroll(true);
+                $window.cordova.plugins.Keyboard.disableScroll(true);
             }
-            if (window.StatusBar) {
-                window.StatusBar.styleDefault();
-            }
-
-            // window.setPageTitle();
-            $rootScope.$on('$stateChangeSuccess', function (event) {
-                // window.setPageTitle();
-                console.debug(event);
-            });
-                // other pieces of code.
-
-            // Function that return a LatLng Object to Map
-            function setPosition(lat, lng) {
-                return new plugin.google.maps.LatLng(lat, lng);
-            }
-
-            var div = document.getElementById("map"),
-                map,
-                cntr;
-
-                // Invoking Map using Google Map SDK v2 by dubcanada
-            if (isMobile) {
-                map = plugin.google.maps.Map.getMap(div, {
-                    'camera': {
-                        'latLng': setPosition(-19.9178713, -43.9603117),
-                        'zoom': 10
-                    }
-                });
-            } else {
-                div = document.getElementById('map');
-                map = google.maps.Map(div);
-                cntr = new google.maps.LatLng(37.422858, -122.085065);
-                map.setCenter(cntr);
-            }
-
-            if (isMobile) {
-                ngModule.run(function ($ionicPlatform, $window) {
-                    $ionicPlatform.ready(function () {
-                        if ($window.cordova && $window.cordova.plugins.Keyboard) {
-                            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                            // for form inputs)
-                            $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-                            // Don't remove this line unless you know what you are doing. It stops the viewport
-                            // from snapping when text inputs are focused. Ionic handles this internally for
-                            // a much nicer keyboard experience.
-                            $window.cordova.plugins.Keyboard.disableScroll(true);
-                        }
-                        if ($window.StatusBar) {
-                            $window.StatusBar.styleDefault();
-                        }
-                    });
-                });
+            if ($window.StatusBar) {
+                $window.StatusBar.styleDefault();
             }
         });
     });
+
+
+} else {
+    mapModule.run(function ($rootScope) {
+        var div,
+            cntr;
+
+        // window.setPageTitle();
+        $rootScope.$on('$stateChangeSuccess', function (event) {
+            // window.setPageTitle();
+            console.debug(event);
+        });
+        div = document.getElementById('map');
+        map = google.maps.Map(div);
+        cntr = new google.maps.LatLng(37.422858, -122.085065);
+        map.setCenter(cntr);
+        // other pieces of code.
+    });
+}
