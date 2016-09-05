6c6ea7c1f9b437a339cf436a397865ce4457a7c7
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2016-09-04 19:05:06 -0700

    Check in before adding branch for browser display

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,123 @@
-// Ionic Starter App
+// Ionic MapLinkr App
+/*global ionic*/
+/*global angular*/
+/*global console*/
+/*global document*/
+/*global plugin*/
+/*global google*/
+/*global ngModule*/
+/*global window*/
+/*global cordova*/
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('maplinkr', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+var modules = [],
+    dependencies = ['ui.router', 'ionic'],
+    isMobile = typeof ionic !== 'undefined' && (ionic.Platform.is("ios") || ionic.Platform.is("android"));
+if (isMobile) {
+    dependencies.push('ngCordova');
+}
+console.debug(modules);
+console.debug(dependencies);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+angular.module('maplinkr', dependencies.concat(modules))
+
+    .config(['$locationProvider', '$compileProvider', '$urlRouterProvider', '$stateProvider',
+        function ($locationProvider, $compileProvider, $urlRouterProvider, $stateProvider) {
+            "use strict";
+            $locationProvider.html5Mode(
+                {
+                    enabled : true,
+                    requireBase : false
+                }
+            ); // enable html5 mode
+            // other pieces of code.
+            $stateProvider.state('map', {
+                url: '/',
+                templateUrl: 'templates/map.html',
+                controller: 'MapCtrl'
+            });
+
+            $urlRouterProvider.otherwise("/");
+        }])
+    .controller('MapCtrl', function ($scope, $state, $cordovaGeolocation) {
+        "use strict";
+        var options = {
+            timeout: 10000,
+            enableHighAccuracy: true
+        };
+
+        $cordovaGeolocation.getCurrentPosition(options).then(function (position) {
+            var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+                mapOptions = {
+                    center: latLng,
+                    zoom: 15,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+            $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+        }, function (error) {
+            console.log("Could not get location");
+            console.debug(error);
+        });
+    })
+    .run(function ($ionicPlatform, $rootScope) {
+        "use strict";
+        $ionicPlatform.ready(function () {
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                // for form inputs)
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                // Don't remove this line unless you know what you are doing. It stops the viewport
+                // from snapping when text inputs are focused. Ionic handles this internally for
+                // a much nicer keyboard experience.
+                cordova.plugins.Keyboard.disableScroll(true);
+            }
+            if (window.StatusBar) {
+                window.StatusBar.styleDefault();
+            }
+
+            // window.setPageTitle();
+            $rootScope.$on('$stateChangeSuccess', function (event) {
+                // window.setPageTitle();
+                console.debug(event);
+            });
+                // other pieces of code.
+
+            // Function that return a LatLng Object to Map
+            function setPosition(lat, lng) {
+                return new plugin.google.maps.LatLng(lat, lng);
+            }
+
+            var div = document.getElementById("map_canvas"),
+
+                // Invoking Map using Google Map SDK v2 by dubcanada
+                map = plugin.google.maps.Map.getMap(div, {
+                    'camera': {
+                        'latLng': setPosition(-19.9178713, -43.9603117),
+                        'zoom': 10
+                    }
+                });
+
+            if (isMobile) {
+                ngModule.run(function ($ionicPlatform, $window) {
+                    $ionicPlatform.ready(function () {
+                        if ($window.cordova && $window.cordova.plugins.Keyboard) {
+                            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                            // for form inputs)
+                            $window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                            // Don't remove this line unless you know what you are doing. It stops the viewport
+                            // from snapping when text inputs are focused. Ionic handles this internally for
+                            // a much nicer keyboard experience.
+                            $window.cordova.plugins.Keyboard.disableScroll(true);
+                        }
+                        if ($window.StatusBar) {
+                            $window.StatusBar.styleDefault();
+                        }
+                    });
+                });
+            }
+        });
+    });
