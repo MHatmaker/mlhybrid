17da19d4f1bcc7c35ff76d55ea1f0f8aa3790bc2
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2016-09-16 11:33:12 -0700

    Adds jade files and jade handling in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,71 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 
+var jade = require('gulp-jade');
+var clean = require('gulp-rimraf');
+
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  jadeIndex: ['./www/views/*.jade'],
+  jadePartials: ['./www/views/partials/*.jade'],
+  jadeTemplates: ['./www/views/templates/*.jade']
 };
 
-gulp.task('default', ['sass']);
+handleError = function(err) {
+  console.log("handleError function called")
+  console.log(err.toString());
+  this.emit('end');
+};
+
+gulp.task('clean', [], function() {
+  console.log("Clean all files in www/js folder");
+
+  return gulp.src("www/js/*", { read: false }).pipe(clean());
+});
+
+gulp.task('default', ['jadeNdx', 'jadeTmplt', 'jadePrt', 'jscopy', 'csscopy', 'sass']);
+
+gulp.task('jadeNdx', function (done) {
+    var YOUR_LOCALS;
+    YOUR_LOCALS = {};
+    console.log("Grab Jade Index file from ");
+    console.log(paths.jadeIndex);
+    gulp.src(paths.jadeIndex).pipe(jade ({
+        locals: YOUR_LOCALS
+    }).on('error', handleError)).pipe(gulp.dest('www')).on('end', done);
+});
+
+gulp.task('jadeTmplt', function (done) {
+    var YOUR_LOCALS;
+    YOUR_LOCALS = {};
+    console.log("Grab Jade Template files from ");
+    console.log(paths.jadeTemplates);
+    gulp.src(paths.jadeTemplates).pipe(jade ({
+        locals: YOUR_LOCALS
+    }).on('error', handleError)).pipe(gulp.dest('www/templates')).on('end', done);
+});
+
+gulp.task('jadePrt', function (done) {
+    var YOUR_LOCALS;
+    YOUR_LOCALS = {};
+    console.log("Grab Jade Template files from ");
+    console.log(paths.jadePartials);
+    gulp.src(paths.jadeTemplates).pipe(jade ({
+        locals: YOUR_LOCALS
+    }).on('error', handleError)).pipe(gulp.dest('www/partials')).on('end', done);
+});
+
+gulp.task('jscopy', function () {
+    return gulp.src(['./**/*'], {
+        base: 'www/public/javascripts'
+    }).pipe(gulp.dest('www/js'));
+});
+
+gulp.task('csscopy', function () {
+    return gulp.src(['www/public/stylesheets/**/*'], {
+        base: 'www/public/stylesheets'
+    }).pipe(gulp.dest('www/css'));
+});
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -27,7 +87,7 @@ gulp.task('sass', function(done) {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.jade, ['jadeNdx', 'jadeTmplt', 'jadePrt', 'jscopy', 'csscopy', 'sass']);
 });
 
 gulp.task('install', ['git-check'], function() {
